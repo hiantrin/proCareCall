e1c8bf06694a2d888a681a8f7fb74b5a04b1dd17
hiantrin <hamzaiantrin1999@gmail.com>
2023-12-05 13:08:35 +0100

    oneOnone

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ export default function App() {
           },
         ],
         scenario: {
-          mode: ZegoUIKitPrebuilt.GroupCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+          mode: ZegoUIKitPrebuilt.OneONoneCall, // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
         },
       });
 
